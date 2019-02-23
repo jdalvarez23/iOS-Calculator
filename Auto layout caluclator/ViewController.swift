53e1c2e07dc14742a7cb66d49f9219aade44f6b0
student19 <student19@student20s-macbook-air.local>
2019-02-23 09:25:47 -0600

    Update 02/23/2019 - Storyboard components added, functions added

--- Auto layout caluclator/ViewController.swift
+++ Auto layout caluclator/ViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // initialize storyboard components
+    @IBOutlet var numberButtons: [UIButton]!
+    @IBOutlet var functionButtons: [UIButton]!
+    @IBOutlet weak var decimalButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // method that executes when a number button is pressed
+    @IBAction func numberButtonsPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    // method that executes when a function button is pressed
+    @IBAction func functionButtonsPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    // method that executes when the decimal button is pressed
+    @IBAction func decimalButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    // method that executes when the clear button is pressed
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+    
 
 
 }
