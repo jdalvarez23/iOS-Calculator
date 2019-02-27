a23d4e10fe3ee1ba1f26f985b3afd52f011d778e
student19 <student19@student20s-macbook-air.local>
2019-02-26 19:57:31 -0600

    Update 02/26/2019 - Added code that properly calculates functions

--- Auto layout caluclator/ViewController.swift
+++ Auto layout caluclator/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     
     var functionsPressedTwice = false
     
+    var finalTotalValue: Double?
+    
     // initialize storyboard components
     @IBOutlet var numberButtons: [UIButton]!
     @IBOutlet var functionButtons: [UIButton]!
@@ -23,6 +25,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var plusMinusButton: UIButton!
     @IBOutlet weak var percentButton: UIButton!
+    @IBOutlet weak var numberDisplay: UILabel!
     
     
     
@@ -73,6 +76,84 @@ class ViewController: UIViewController {
     // method that executes when a function button is pressed
     @IBAction func functionButtonsPressed(_ sender: UIButton) {
         
+        let mathExpression = sender.title(for: .normal)! // set value to button title
+        
+        print("Math expression called:", mathExpression)
+        
+        // check if there is a value in finalTotalValue variable and if calculationArray is empty
+        if (finalTotalValue != nil && (calculationArray.count == 0)) {
+            
+            // there is a value in the finalTotalValue variable AND the calculation array IS empty so the user wants to add on a calculation to the total value
+            print("User wants to ADD on to the total value")
+            
+            let totalValue = finalTotalValue!
+            
+            let formattedValue = String(totalValue)
+            
+            print("MATH EXPRESSIONS:", executeCalculation)
+            
+            calculationArray.append("")
+            calculationArray.insert(formattedValue, at: 0)
+            
+            // insert calculations to executeCalculation array at index 0
+            executeCalculation.insert(mathExpression, at: 0)
+            
+            
+        } else {
+            
+            // there is no value in the finalTotalValue variable OR the calculationArray IS NOT empty so the user wants to calculate a new expression
+            print("User wants NEW expression")
+            
+            calculationArray.append("") // create empty value in calculation array
+        
+            // execute the total expression
+            if (mathExpression == "=") {
+                print("Executing TOTAL math expression")
+                
+                // call method that calculates the total
+                total(firstNumber: calculationArray[0], secondNumber: calculationArray[1], expression: executeCalculation[0])
+                
+                executeCalculation.removeAll() // remove all calculations inserted into executeCalculation array
+                
+                functionsPressedTwice = false // default back to false
+                
+                
+                
+            } else {
+                
+                // insert calculations to executeCalculation array at index 0
+                executeCalculation.insert(mathExpression, at: 0)
+                
+                // check for second time function pressing
+                
+                // execute if function is pressed for the first time
+                if (!functionsPressedTwice) {
+                    functionsPressedTwice = true // set value to true
+                    
+                // execute if function has been pressed and user pressed again
+                } else {
+                    print("Second time executing functions!")
+                    
+                    // call method to calculate total and push total value to index 0
+                    total(firstNumber: calculationArray[0], secondNumber: calculationArray[1], expression: executeCalculation[0])
+            
+                    if finalTotalValue != nil {
+                        calculationArray.append("")
+                        calculationArray.insert(String(finalTotalValue!), at: 0)
+                        
+                    }
+                    
+                    
+                }
+                
+ 
+            }
+        
+        
+        }
+        
+        /*
+        
         /* Create new value in calculationsArray */
         
         calculationArray.append("")
@@ -105,7 +186,7 @@ class ViewController: UIViewController {
                 // call method to calculate total and push total value to index 0
                 total(firstNumber: calculationArray[0], secondNumber: calculationArray[1], calculation: executeCalculation[0])
             }
-        }
+        } */
         
         
         
@@ -135,7 +216,9 @@ class ViewController: UIViewController {
     }
     
     // method that calculates total
-    func total(firstNumber: String, secondNumber: String, calculation: String) {
+    func total(firstNumber: String, secondNumber: String, expression: String) {
+        
+        print("Numbers passed into function:", firstNumber, secondNumber)
         
         let formattedFirstNum = Double(firstNumber) ?? 0
         let formattedSecondNum = Double(secondNumber) ?? 0
@@ -143,7 +226,7 @@ class ViewController: UIViewController {
         var total: Double = 0
         
         // conditional statement to execute a specific function based on calculation passed
-        switch calculation {
+        switch expression {
         case "÷":
             // Divide
             total = formattedFirstNum / formattedSecondNum
@@ -163,12 +246,23 @@ class ViewController: UIViewController {
         print("The total is:", total)
         
         // insert call to update user interface here
-        
+
         /* [NOTE] We can actually remove the following two lines and simply display the number in the label and if the user justs presses a function button then we can add the number in the label back to the calculations array */
         
-        calculationArray[0] = String(total)
+       // calculationArray[0] = String(total)
         
-        calculationArray.remove(at: 1)
+        // calculationArray.remove(at: 1)
+        
+        finalTotalValue = total // set value to calculated total
+        
+        calculationArray.removeAll() // remove all values in array
+        
+    }
+    
+    func updateUserInterface(value: Double) {
+        
+        // check to see if value (as a Double) is a whole number or a decimal number
+        
         
     }
     
